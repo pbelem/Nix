0a342abe2a27c1f4c936aee1a8dfc7af04d76c4b
pbelem <belem@tuta.io>
2026-05-02 10:41:34 -0300

    initial integration

--- home.nix
+++ home.nix
@@ -180,6 +180,8 @@ programs.kitty = {
     zathuraPkgs.zathura_pdf_mupdf
     zathuraPkgs.zathura_djvu
     zathuraPkgs.zathura_cb
+
+    monocraft
   ];
 
   # Git configuration (name and email)
@@ -201,7 +203,154 @@ programs.kitty = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    extraConfig = builtins.readFile ./hyprland.conf;
+    
+    settings = {
+      "$mainMod" = "SUPER";
+      "$ipc" = "qs -c noctalia-shell ipc call";
+
+      monitor = [
+        ", 1920x1080@320, auto, 1"
+      ];
+
+      env = [
+        "XCURSOR_SIZE,32"
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "LIBVA_DRIVER_NAME,radeonsi"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_QPA_PLATFORMTHEME,qt6ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "GDK_SCALE,1"
+        "MOZ_ENABLE_WAYLAND,1"
+        "OZONE_PLATFORM,wayland"
+        "NIXOS_OZONE_WL,1"
+      ];
+
+      input = {
+        kb_layout = "us";
+        kb_variant = "intl";
+        follow_mouse = 1;
+        sensitivity = 0;
+        touchpad = {
+          natural_scroll = false;
+        };
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(ffb86bff) rgba(ff79c6ff) 45deg";
+        "col.inactive_border" = "rgba(44475aff)";
+        resize_on_border = true;
+        layout = "dwindle";
+      };
+
+      decoration = {
+        rounding = 20;
+        rounding_power = 2;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 2;
+          vibrancy = 0.1696;
+          new_optimizations = true;
+        };
+      };
+
+      animations = {
+        enabled = true;
+        bezier = [
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+          "menu_decel, 0.1, 1, 0, 1"
+          "menu_accel, 0.38, 0.04, 1, 0.07"
+        ];
+        animation = [
+          "windows, 1, 3, md3_decel, popin 60%"
+          "border, 1, 10, default"
+          "fade, 1, 3, md3_decel"
+          "workspaces, 1, 7, menu_decel, slide"
+        ];
+      };
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+        force_default_wallpaper = 0;
+        vfr = true;
+        vrr = 1;
+      };
+
+      bind = [
+        # --- Applications ---
+        "$mainMod, RETURN, exec, kitty"
+        "$mainMod, DELETE, killactive"
+        "$mainMod, E, exec, kitty -e yazi"
+        "$mainMod, ESC, exec, kitty -e btop"
+        "$mainMod, B, exec, brave"
+
+        # --- Windows & Workspaces ---
+        "$mainMod, T, togglefloating"
+        "$mainMod, Home, fullscreen, 0"
+        
+        # Minimizing using a special workspace
+        "$mainMod, End, movetoworkspacesilent, special:minimized" # Sends the window and focuses on the background
+        "$mainMod SHIFT, End, togglespecialworkspace, minimized"  # Brings the "minimized" area back to the screen
+
+        # Move focus
+        "$mainMod, left, movefocus, l"
+        "$mainMod, right, movefocus, r"
+        "$mainMod, up, movefocus, u"
+        "$mainMod, down, movefocus, d"
+
+        # Workspace Navigation (+1 and -1)
+        "$mainMod, Page_Up, workspace, +1"
+        "$mainMod, Page_Down, workspace, -1"
+
+        # Absolute Workspaces (1-0)
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        # --- Noctalia Shell (IPC Calls) ---
+        "$mainMod, SPACE, exec, $ipc launcher toggle"
+        "$mainMod, I, exec, $ipc settings toggle"
+        "$mainMod, period, exec, $ipc launcher emoji" # The '.' key is called 'period'
+        
+        "$mainMod, F12, exec, $ipc brightness increase"
+        "$mainMod, F11, exec, $ipc brightness decrease"
+        "$mainMod, F10, exec, $ipc volume increase"
+        "$mainMod, F9, exec, $ipc volume decrease"
+        "$mainMod, INSERT, exec, $ipc volume muteOutput"
+
+        # Noctalia Plugins
+        "$mainMod, L, exec, $ipc lockScreen lock"
+        "$mainMod, W, exec, $ipc wallpaper random" 
+
+        # --- Others ---
+        ", Print, exec, hyprshot -m region --clipboard-only"
+      ];
+
+      # Mouse Binds
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
   };
 
   gtk.enable = true;
